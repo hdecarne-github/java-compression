b7929b50fd27cf5ca2f92a3191696298af2ae374
Holger <hdecarne@gmail.com>
2016-05-21 21:40:54 +0200

    Setup of base classes.

---
+++ de.carne.nio.compression/src/main/java/de/carne/nio/compression/Compression.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2016 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.nio.compression;
+
+/**
+ * Basic interface for compression engines.
+ */
+public abstract class Compression {
+
+	private long processingTime = 0l;
+
+	private long totalIn = 0l;
+
+	private long totalOut = 0l;
+
+	/**
+	 * Get the compression name.
+	 *
+	 * @return The he compression name.
+	 */
+	public abstract String name();
+
+	/**
+	 * Reset the compression engine to it's initial state.
+	 */
+	public synchronized void reset() {
+		this.processingTime = 0l;
+		this.totalIn = 0l;
+		this.totalOut = 0l;
+	}
+
+	/**
+	 * Get the time (in milliseconds) spent in this engine since the last call
+	 * to {@linkplain #reset()}.
+	 *
+	 * @return The time (in milliseconds) spent in this engine since the last
+	 *         call to {@linkplain #reset()}.
+	 */
+	public synchronized final long processingTime() {
+		return this.processingTime;
+	}
+
+	/**
+	 * Get the number of bytes consumed by this engine since the last call to
+	 * {@linkplain #reset()}.
+	 *
+	 * @return The number of bytes consumed by this engine since the last call
+	 *         to {@linkplain #reset()}.
+	 */
+	public synchronized final long totalIn() {
+		return this.totalIn;
+	}
+
+	/**
+	 * Get the number of bytes emitted by this engine since the last call to
+	 * {@linkplain #reset()}.
+	 *
+	 * @return The number of bytes emitted by this engine since the last call to
+	 *         {@linkplain #reset()}.
+	 */
+	public synchronized final long totalOut() {
+		return this.totalOut;
+	}
+
+	/**
+	 * Record the start time every time engine processing begins.
+	 * <p>
+	 * Derived class have to call this function to make sure engine statistics
+	 * are properly tracked.
+	 * </p>
+	 *
+	 * @return The recorded start time, which should be submitted to the
+	 *         {@linkplain #endProcessing(long, long, long)} call when engine
+	 *         processing ends.
+	 */
+	protected synchronized final long beginProcessin() {
+		return System.currentTimeMillis();
+	}
+
+	/**
+	 * Record the processing time and input/ouput bytes time every time engine
+	 * processing ends.
+	 * <p>
+	 * Derived class have to call this function to make sure engine statistics
+	 * are properly tracked.
+	 * </p>
+	 *
+	 * @param beginTime The begin time as returned by
+	 *        {@linkplain #beginProcessin()}.
+	 * @param in The number of consumed bytes.
+	 * @param out The number of emitted bytes.
+	 */
+	protected synchronized final void endProcessing(long beginTime, long in, long out) {
+		long currentTime = System.currentTimeMillis();
+
+		assert beginTime <= currentTime;
+		assert in >= 0;
+		assert out >= 0;
+
+		this.processingTime += currentTime - beginTime;
+		this.totalIn += in;
+		this.totalOut += out;
+	}
+
+}
